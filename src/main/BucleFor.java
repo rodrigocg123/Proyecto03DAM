651e67f9ae0a3d721f0429b770f5ddfa1328a135
rodrigocg123 <rocasgut@gmail.com>
2025-10-09 12:23:55 +0200

    correcion de paquete en bucleSwitch y creacion de clase BucleFor

---
+++ src/main/BucleFor.java
@@ -0,0 +1,8 @@
+public class BucleFor {
+    public static void main(String[] args) {
+        int numeroTabla = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(numeroTabla + " x " + i + " = " + (numeroTabla * i));
+        }
+    }
+}
--- src/main/BucleSwitch.java
+++ src/main/BucleSwitch.java
@@ -1,4 +1,4 @@
-package main;
+
 
 public class BucleSwitch {
     public static void main(String[] args) {
